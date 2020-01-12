2effbc38995a6117355ea3e3f44f9b8f0ca405c4
Matt Kilgore <tankerkiller125@gmail.com>
2020-01-12 12:29:27 -0500

    Update translation strings

--- js/src/admin/components/PerspectiveSettingsModal.js
+++ js/src/admin/components/PerspectiveSettingsModal.js
@@ -12,19 +12,19 @@ import Switch from 'flarum/components/Switch';
 
 export default class PerspectiveSettingsModal extends SettingsModal {
   title() {
-    return app.translator.trans('perspective.admin.settings.title');
+    return app.translator.trans('tank-perspective.admin.settings.title');
   }
 
   form() {
     return [
       m('.Form-group', [
-        m('label', app.translator.trans('perspective.admin.settings.field.api_key')),
+        m('label', app.translator.trans('tank-perspective.admin.settings.field.api_key')),
         m('input[type=text].FormControl', {
           bidi: this.setting('perspective.api_key')
         })
       ]),
       m('.Form-group', [
-        m('label', app.translator.trans('perspective.admin.settings.field.threshold')),
+        m('label', app.translator.trans('tank-perspective.admin.settings.field.threshold')),
         m('input[type=number][max=100].FormControl', {
           bidi: this.setting('perspective.threshold')
         })
@@ -33,56 +33,56 @@ export default class PerspectiveSettingsModal extends SettingsModal {
         m('label', Switch.component({
           state: this.setting('perspective.usemax')() > 0,
           onchange: this.setting('perspective.usemax'),
-          children: app.translator.trans('perspective.admin.settings.usemax'),
+          children: app.translator.trans('tank-perspective.admin.settings.usemax'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.useexperimental')() > 0,
           onchange: this.setting('perspective.useexperimental'),
-          children: app.translator.trans('perspective.admin.settings.useexperimental'),
+          children: app.translator.trans('tank-perspective.admin.settings.useexperimental'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.models.toxicity')() > 0,
           onchange: this.setting('perspective.models.toxicity'),
-          children: app.translator.trans('perspective.admin.settings.field.toxicity'),
+          children: app.translator.trans('tank-perspective.admin.settings.field.toxicity'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.models.threat')() > 0,
           onchange: this.setting('perspective.models.threat'),
-          children: app.translator.trans('perspective.admin.settings.field.threat'),
+          children: app.translator.trans('tank-perspective.admin.settings.field.threat'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.models.profanity')() > 0,
           onchange: this.setting('perspective.models.profanity'),
-          children: app.translator.trans('perspective.admin.settings.field.profanity'),
+          children: app.translator.trans('tank-perspective.admin.settings.field.profanity'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.models.sexually_explicit')() > 0,
           onchange: this.setting('perspective.models.sexually_explicit'),
-          children: app.translator.trans('perspective.admin.settings.field.sexually_explicit'),
+          children: app.translator.trans('tank-perspective.admin.settings.field.sexually_explicit'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.models.flirtation')() > 0,
           onchange: this.setting('perspective.models.flirtation'),
-          children: app.translator.trans('perspective.admin.settings.field.flirtation'),
+          children: app.translator.trans('tank-perspective.admin.settings.field.flirtation'),
         })),
       ]),
       m('.Form-group', [
         m('label', Switch.component({
           state: this.setting('perspective.donotstore')() > 0,
           onchange: this.setting('perspective.donotstore'),
-          children: app.translator.trans('perspective.admin.settings.donotstore'),
+          children: app.translator.trans('tank-perspective.admin.settings.donotstore'),
         })),
       ]),
     ]
